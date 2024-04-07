245c4515b255180e36dac70960d663a7357e4bb9
teng.luo <teng.luo@100credit.com>
2024-04-07 20:02:40 +0800

    fix

--- deploy.sh
+++ deploy.sh
@@ -3,16 +3,21 @@ mkdir -p .deploy/.git
 cp -r .git .deploy/
 cd .deploy
 git checkout gh-pages
+git pull
 rm -rf `ls`
 cd ../
 hexo clean && hexo generate
-cp -r ./public/* .deploy/
-cd .deploy
-git add .
-git commit -m 'deploy'
-git push
-cd ../
-rm -rf .deploy
+if [ $? -eq 0 ]; then
+    cp -r ./public/* .deploy/
+	cd .deploy
+	git add .
+	git commit -m 'deploy'
+	git push
+	cd ../
+	rm -rf .deploy
+else
+    echo "failed"
+fi
 
 
 # pwd=$(basename "$PWD")
