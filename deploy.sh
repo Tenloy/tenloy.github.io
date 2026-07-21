0e2a25f0a5a3736215e3d7b5140f6b172c81c45d
luoteng <luoteng@kuaishou.com>
2026-07-21 20:28:24 +0800

    Revert "deploy"
    This reverts commit 1ef03b1681457f0357c18354ec48ffca612af77d.

---
+++ deploy.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+work_dir=$(
+    cd "$(dirname "$0")"
+    pwd
+)
+
+cd $work_dir
+pwd
+
+mkdir -p .deploy/.git
+cp -r .git .deploy/
+cd .deploy
+git checkout gh-pages && git pull
+rm -rf `ls`
+cd ../
+hexo clean && hexo generate
+if [ $? -eq 0 ]; then
+    cp -r ./public/* .deploy/
+	cd .deploy
+	git add .
+	git commit -m 'deploy'
+	git push
+	cd ../
+	rm -rf .deploy
+else
+    echo "failed"
+fi
+
+
+# pwd=$(basename "$PWD")
+# tempdir="../.temp"
+# cpdir=$tempdir"/"$pwd"/"
+# rm -rf $tempdir
+# mkdir -p $cpdir
+# cp -r ./ $cpdir
+# cd $cpdir
+# hexo clean && hexo generate
+# cp -r ./public ../
+# git checkout gh-pages
+# rm -rf `ls`
+# cp -r ../public/* ./
+# git add . && git commit -m 'deploy' && git push
+# cd ../../$pwd
+# rm -rf $tempdir
